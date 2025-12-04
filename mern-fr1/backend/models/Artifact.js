1d4d37d3ac7f14beb339c4cf886a4950194f8c93
Python-401 <sahil12345678989@gmail.com>
2025-12-04 15:45:29 +0600

    Delete mern-fr1/backend/models directory

--- mern-fr1/backend/models/Artifact.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const artifactSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  imageUrl: { type: String, required: true }
-});
-
-module.exports = mongoose.model("Artifact", artifactSchema);
